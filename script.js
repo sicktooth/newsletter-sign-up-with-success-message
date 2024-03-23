80ab51fe2e935206a2a840954eaece75fae17884
sicktooth <sosaronungwe@gmail.com>
2024-03-23 23:54:33 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function validate(e) {
-
+    var main = document.querySelector("main"),
+        success = document.querySelector("article");
     e.preventDefault(); // prevents auto submission i.e email= in the url
 
     // logic in here 
 
     var mail = document.getElementById("email").value;
-    var regex = /^([a-zA-Z0-9\._]+)@(gmail|proton|outlook|hotmail|yahoo|mail)+.([a-z]+)(.[a-z]+)?$/;
+    var regex = /^([a-zA-Z0-9\._]+)@(gmail|proton|outlook|hotmail|yahoo|mail|aol|zoho|icloud|gmx|yandex|hushmail|fastmail|tutanota)+.([a-z]+)(.[a-z]+)?$/;
 
     if (mail.match(regex)) {
-        alert("Valid email address");
+        main.style.display = "none";
+        success.style.display = "block";
     } else {
         alert("Invalid email address");
     }
