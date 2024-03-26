9960f08453b86055336a27234a4ec5fe8173f123
sicktooth <sosaronungwe@gmail.com>
2024-03-26 04:20:33 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 const main = $("main"),
     success = $("article"),
-    replace = document.querySelector("b")
-    errorLabel = document.querySelector(".error__message"),
-    errorInput = document.querySelector("#email");
+    replace = $("b"),
+    errorLabel = $(".error__message"),
+    errorInput = $("#email");
     
 function validate(e) {
     
     e.preventDefault(); // prevents auto submission i.e email= in the url
 
     // logic in here 
-    var mail = document.getElementById("email").value;
+    var mail = $("#email").val();
     var regex = /^([a-zA-Z0-9\._]+)@(gmail|proton|outlook|hotmail|yahoo|mail|aol|zoho|icloud|gmx|yandex|hushmail|fastmail|tutanota)+.([a-z]+)(.[a-z]+)?$/;
 
     if (mail.match(regex)) {
         main.hide().fadeOut(1000);
         success.fadeIn(1000);
-        replace.innerHTML = mail;
-        errorLabel.style.display = "none";
+        replace.text(mail);
+        errorLabel.css("display", "none");
     } else {
-        errorLabel.style.display = "block";
-        errorInput.classList.add("error__input");
+        errorLabel.css("display", "block");
+        errorInput.addClass("error__input");
     }
 
 }
 function reset(){
-    main.css("display", "grid");
-    success.fadeOut("slow");
-    errorInput.value = "";
+    success.fadeOut(1000);
+    main.fadeIn(1000);
+    errorInput.val("");
+    // window.location.reload();
 }
-var form = document.querySelector("#main__form"),
-    dismiss = document.querySelector(".button");
-form.addEventListener("submit",validate);
-dismiss.addEventListener("click",reset);
+var form = $("#main__form"),
+    dismiss = $(".button");
+form.on("submit",validate);
+dismiss.on("click",reset);
