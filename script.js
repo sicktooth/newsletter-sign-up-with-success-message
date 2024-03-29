305a97f4c1506e5a283bd81efcf36a679f990090
sicktooth <sosaronungwe@gmail.com>
2024-03-29 06:08:00 +0100

    update script

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ const main = $("main"),
     success = $("article"),
     replace = $("b"),
     errorLabel = $(".error__message"),
-    errorInput = $("#email");
+    errorInput = $("#email"),
+    input = $("#email"),
+    form = $("#main__form"),
+    dismiss = $(".button"),
+    regex = /^([\w\._]+)@(gmail|proton|outlook|hotmail|yahoo|mail|aol|zoho|icloud|gmx|yandex|hushmail|fastmail|tutanota)+.([a-z]+)(.[a-z]+)?$/; // only popular email services are supported
     
 function validate(e) {
-    
+
     e.preventDefault(); // prevents auto submission i.e email= in the url
 
     // logic in here 
-    var mail = $("#email").val();
-    var regex = /^([\w\._]+)@(gmail|proton|outlook|hotmail|yahoo|mail|aol|zoho|icloud|gmx|yandex|hushmail|fastmail|tutanota)+.([a-z]+)(.[a-z]+)?$/; // only popular email services are supported
-
+    var mail = input.val();
     if (mail.match(regex)) { // checks the input against the regex
         main.hide().fadeOut(1000);
         success.fadeIn(1000);
@@ -21,7 +23,13 @@ function validate(e) {
         errorLabel.css("display", "block");
         errorInput.addClass("error__input");
     }
-
+}
+const inputValidator = () => {
+    const inputValue = input.val();
+    if (inputValue.match(regex)) {
+        errorLabel.css("display", "none");
+        errorInput.removeClass("error__input");
+    }
 }
 function reset(){
     success.fadeOut(1000);
@@ -29,7 +37,9 @@ function reset(){
     errorInput.val("");
     errorInput.removeClass("error__input");
 }
-var form = $("#main__form"),
-    dismiss = $(".button");
+
+//Event Listeners
+    
+input.on("input",inputValidator);
 form.on("submit",validate);
 dismiss.on("click",reset);
